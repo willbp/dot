6565aae4469058d14f368f39192cb1743bd34558
willbp <williambortoluzzipereira@gmail.com>
2025-03-19 10:37:24 -0300

    Configurando pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'yi154t',
   e2e: {
     baseUrl: "https://automationpratice.com.br",
     setupNodeEvents(on, config) {
