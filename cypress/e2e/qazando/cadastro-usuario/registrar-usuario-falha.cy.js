215f357e44c755947ed4907c97bba54ef298c108
willbp <williambortoluzzipereira@gmail.com>
2025-03-20 09:46:00 -0300

    tentar cadastrar somente com nome preenchido

--- cypress/e2e/qazando/cadastro-usuario/registrar-usuario-falha.cy.js
+++ cypress/e2e/qazando/cadastro-usuario/registrar-usuario-falha.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 describe("Validação de erros no cadastro de usuário", () => {
 
     const elementos = {
@@ -26,5 +28,19 @@ describe("Validação de erros no cadastro de usuário", () => {
         cy.get(elementos.messages.errorName)
             .should('be.visible')
             .should('contain.text', 'O campo nome deve ser prenchido');
+    });
+
+    it("Deve exibir erro ao tentar cadastrar apenas com campo Nome preenchido", () => {
+        cy.get(elementos.fields.name)
+            .should('be.visible')
+            .type(faker.person.fullName())
+
+        cy.get(elementos.buttons.register)
+            .should('be.visible')
+            .click()
+
+        cy.get(elementos.messages.errorName)
+            .should('be.visible')
+            .should('contain.text', 'O campo e-mail deve ser prenchido corretamente');
     });
 });
