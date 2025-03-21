0de737b19de7301d0c7c72a3845886a473a2780a
willbp <williambortoluzzipereira@gmail.com>
2025-03-21 12:21:14 -0300

    refatoracao estrutural e melhorias

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://automationpratice.com.br",
-    defaultCommandTimeout: 5000,
+    defaultCommandTimeout: 6000,
     setupNodeEvents(on, config) {
     },
   },
